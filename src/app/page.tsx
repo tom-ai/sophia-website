8f854329fc19a0c4f39878cc9287bba75362ee7d
Tom Harris <tomharris3@me.com>
2023-11-10 18:25:36 +0000

    add header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,26 @@
+'use client';
+
+import Head from 'next/head';
+import { Link } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <BrowserRouter>
+      <Header />
+    </BrowserRouter>
+  );
+}
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <Link to={''}>
+          <h1>My Name</h1>
+        </Link>
+      </nav>
+      <p>Session Musician</p>
+    </header>
+  );
 }
