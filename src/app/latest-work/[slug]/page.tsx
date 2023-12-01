1824f544c9b915d047a8038e64dd2f8a62683a3e
Tom Harris <tomharris3@me.com>
2023-12-01 13:25:59 +0000

    make all page

---
+++ src/app/latest-work/[slug]/page.tsx
@@ -0,0 +1,78 @@
+import { gql } from '@apollo/client';
+import createApolloClient from '@/app/utils/apollo-client';
+import { notFound } from 'next/navigation';
+
+export default async function LatestWork({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  async function getData(slug: string) {
+    const client = createApolloClient();
+    const { data } = await client.query({
+      variables: { slug },
+      query: gql`
+        query collaboratorBySlug($slug: String) {
+          collaborator(filter: { slug: { eq: $slug } }) {
+            id
+            slug
+            name
+            posts {
+              id
+              message
+              date
+              link
+              collaborators {
+                id
+                name
+              }
+            }
+          }
+        }
+      `,
+    });
+
+    return data;
+  }
+
+  const { collaborator } = await getData(params.slug);
+
+  if (!collaborator) return notFound();
+
+  return (
+    <section>
+      <h3>{collaborator.name}</h3>
+      <div className="grid">
+        {collaborator.posts.map((post: any) => (
+          <article key={post.id}>
+            <header>
+              <time>{post.date}</time>
+              <p>
+                <small>
+                  {post.collaborators.map(
+                    (collaborator: any) => collaborator.name
+                  )}
+                </small>
+              </p>
+            </header>
+            <blockquote>
+              <strong>{post.message}</strong>
+            </blockquote>
+
+            <br />
+            <footer>
+              <a role="button" href={post.link} target="_blank">
+                Watch/Listen
+              </a>
+            </footer>
+          </article>
+        ))}
+      </div>
+    </section>
+  );
+}
+
+// get params, pass it to child component
+
+// if params is null, render allPosts
+// if params is not not null, render allPostsByCollaborator
