803d821c7875724edb9b56ee1b1f25ee6a4bcbf4
Tom Harris <tomharris3@me.com>
2023-12-01 21:09:27 +0000

    install harmony color palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
+import harmonyPalette from '@evilmartians/harmony/tailwind';
 
 const config: Config = {
   content: [
@@ -7,14 +8,8 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+    colors: { harmonyPalette },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
