2cc1f3bf7b12a8bb9c66a6dd8ee7897b0e6f09a1
Tom Harris <tomharris3@me.com>
2023-12-04 19:21:32 +0000

    add post list component

---
+++ src/app/components/PostList.tsx
@@ -0,0 +1,50 @@
+import {
+  Card,
+  CardHeader,
+  Chip,
+  Link,
+  Divider,
+  CardBody,
+  CardFooter,
+} from "@nextui-org/react";
+import { Post } from "../types/types";
+
+type PostListProps = {
+  posts: Post[];
+};
+
+export default function PostList({ posts }: PostListProps) {
+  return posts.map((post: Post) => (
+    <Card>
+      <article>
+        <CardHeader>
+          <div className="flex gap-2">
+            <time>{post.date}</time>
+            {post.collaborators.map((collaborator) => (
+              <Chip
+                key={collaborator.id}
+                color="secondary"
+                size="sm"
+                variant="flat"
+                as={Link}
+                href={`/latest-work/${collaborator.slug}`}
+              >
+                {collaborator.name}
+              </Chip>
+            ))}
+          </div>
+        </CardHeader>
+        <Divider />
+        <CardBody>
+          <blockquote>{post.message}</blockquote>
+        </CardBody>
+        <Divider />
+        <CardFooter>
+          <Link isExternal showAnchorIcon href={post.link}>
+            Visit content on YouTube.
+          </Link>
+        </CardFooter>
+      </article>
+    </Card>
+  ));
+}
