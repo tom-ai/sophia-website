533f4f6e787df6501cbb5f078747c33dce539b75
Tom Harris <tomharris3@me.com>
2023-12-24 14:51:42 +0000

    update the client function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,41 +4,39 @@ import Image from "next/image";
 import { Collaborators } from "./components/Collaborators";
 import { Button, Link } from "@nextui-org/react";
 import SectionHeader from "./components/SectionHeader";
+import { performRequest } from "./lib/datocms";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <main>
-        <Collaborators />
-        <About />
+        {/* <Collaborators />
+        <About /> */}
       </main>
     </>
   );
 }
 
 async function Hero() {
-  async function getData() {
-    const client = createApolloClient();
-    const { data } = await client.query({
-      query: gql`
-        query HeroSection {
-          heroSection {
-            title
-            body
-            ctaText
-            image {
-              url
-              title
-              alt
-            }
-          }
-        }
-      `,
-    });
-    return data;
-  }
-  const { heroSection } = await getData();
+  const PAGE_CONTENT_QUERY = `
+  query Hero {
+    heroSection {
+      title
+      body
+      ctaText
+      image {
+        url
+        title
+        alt
+      }
+    }
+  }`;
+
+  const { heroSection } = await performRequest({
+    query: PAGE_CONTENT_QUERY,
+    revalidate: false,
+  });
 
   return (
     <header className="py-12 md:grid md:h-screen md:grid-cols-2 md:grid-rows-1 md:gap-6 2xl:h-[50vh]">
--- src/app/utils/apollo-client.ts
+++ src/app/utils/apollo-client.ts
@@ -1,14 +1,14 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const createApolloClient = () => {
   return new ApolloClient({
-    uri: 'https://graphql.datocms.com/',
+    uri: "https://graphql.datocms.com/",
     // post
     cache: new InMemoryCache(),
     headers: {
-      authorization: process.env.API_TOKEN || '',
-      'Content-Type': 'application/json',
-      Accept: 'application/json',
+      authorization: process.env.NEXT_DATOCMS_API_TOKEN || "",
+      "Content-Type": "application/json",
+      Accept: "application/json",
     },
   });
 };
