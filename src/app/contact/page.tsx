375dad64fb6fcf197695b6658c6ada087ef87c4f
Tom Harris <tomharris3@me.com>
2023-12-28 15:05:32 +0000

    remove nested section

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,13 +4,11 @@ import PageHeader from "../components/PageHeader";
 export default function Contact() {
   return (
     <>
-      <PageHeader direction="center" title="Contact">
+      <PageHeader direction="center" title="Contact Sophia">
         Drop me a message!
       </PageHeader>
       <main className="md:flex md:justify-center">
-        <section className="py-12 md:w-2/3">
-          <ContactForm />
-        </section>
+        <ContactForm />
       </main>
     </>
   );
