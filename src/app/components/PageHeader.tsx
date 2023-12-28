52683c07cd6d8fffff9fec92fb7733242ae29e66
Tom Harris <tomharris3@me.com>
2023-12-28 09:43:30 +0000

    update pageHeader component for direction

--- src/app/components/PageHeader.tsx
+++ src/app/components/PageHeader.tsx
@@ -1,11 +1,16 @@
 type PageHeaderProps = {
   title: string;
   children?: React.ReactNode;
+  direction?: "left" | "right" | "center";
 };
 
-export default function PageHeader({ title, children }: PageHeaderProps) {
+export default function PageHeader({
+  title,
+  children,
+  direction = "left",
+}: PageHeaderProps) {
   return (
-    <header className="py-12">
+    <header className={`py-12 text-${direction}`}>
       <hgroup>
         <h2 className="text-3xl font-bold">{title}</h2>
         {children}
