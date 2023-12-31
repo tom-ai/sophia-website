cc7750cffb2a5ea5af04bb03bb55c00e1591623a
Tom Harris <tomharris3@me.com>
2023-12-31 22:34:31 +0000

    remove header from config

--- next.config.js
+++ next.config.js
@@ -16,19 +16,6 @@ const nextConfig = {
       },
     ],
   },
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "X-Robots-Tag",
-            value: "noindex",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
