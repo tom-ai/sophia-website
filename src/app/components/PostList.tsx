6dec9815e395b239b75692e18b40b4eae514a65b
Tom Harris <tomharris3@me.com>
2023-12-31 23:16:06 +0000

    add thumnbail to post, redesign a bit

--- src/app/components/PostList.tsx
+++ src/app/components/PostList.tsx
@@ -17,34 +17,44 @@ export default function PostList({ posts }: PostListProps) {
   return posts.map((post: any) => (
     <Card className="">
       <article>
-        <CardHeader>
-          <div className="flex gap-2">
-            <time>{post.date}</time>
-            {post.collaborators.map((collaborator: any) => (
-              <Chip
-                key={collaborator.id}
-                color="secondary"
-                size="sm"
-                variant="flat"
-                as={Link}
-                href={`/latest-work/${collaborator.slug}`}
-              >
-                {collaborator.name}
-              </Chip>
-            ))}
+        <CardBody className="flex flex-row gap-3">
+          <div className="relative flex aspect-video h-28">
+            <Image
+              src={post.embed.thumbnailUrl}
+              alt={post.embed.title}
+              fill
+              className="object-cover"
+            />
           </div>
-        </CardHeader>
-        <Divider />
-        <CardBody>
-          <blockquote>{post.message}</blockquote>
-          {/* <Image src={} /> */}
+
+          <div className="flex flex-col justify-between space-y-4">
+            <div className="flex flex-col">
+              <time>
+                <small className="text-primary-foreground/60">
+                  {post.date}
+                </small>
+              </time>
+              <blockquote>{post.message}</blockquote>
+            </div>
+            <div className="flex flex-wrap gap-2">
+              {post.collaborators.map((collaborator: any) => (
+                <Chip
+                  key={collaborator.id}
+                  color="secondary"
+                  size="sm"
+                  variant="flat"
+                  as={Link}
+                  href={`/latest-work/${collaborator.slug}`}
+                >
+                  {collaborator.name}
+                </Chip>
+              ))}
+            </div>
+            <Link isExternal showAnchorIcon href={post.link}>
+              Visit content on YouTube.
+            </Link>
+          </div>
         </CardBody>
-        <Divider />
-        <CardFooter>
-          <Link isExternal showAnchorIcon href={post.link}>
-            Visit content on YouTube.
-          </Link>
-        </CardFooter>
       </article>
     </Card>
   ));
